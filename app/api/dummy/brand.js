1b07cebb005b9b4abe4b680f35ae22f17bf5bfc1
jassem-lab <jassemgaaloul123@gmail.com>
2022-06-01 10:56:17 +0100

    initial commit

---
+++ app/api/dummy/brand.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Gestion des evenements',
+  desc: 'Gestion des evenements',
+  prefix: 'Macsi Centre',
+  footerText: 'Macsi Centre Pro All Rights Reserved 2022',
+  logoText: 'Gestion des evenements',
+};
